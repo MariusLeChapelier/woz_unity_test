dfb97d3d64a552f2f0e8289ac8b0c16539c7ea9e
Marius Le Chapelier <mlechape@ptb-02007553.paris.inria.fr>
2023-11-07 15:44:38 +0100

    #3 creating the basic app

---
+++ App/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+  
+var cpt = 0;
+
+app.get('/', function (req, res) {
+    res.send('Hello World <form action="/dance" method="post"><input type="submit" value="Dance"></form> <form action="/idle" method="post"><input type="submit" value="Idle"></form>');
+});
+// app.post('/', function (req, res) {
+//     res.send('base <form action="/dance" method="post"><input type="submit" value="Dance"></form> <form action="/idle" method="post"><input type="submit" value="Idle"></form>');
+// });
+
+app.post('/dance', function (req, res) {
+    // cpt = cpt + 1;
+    // console.log(cpt);
+    res.send('Dance <form action="/idle" method="post"><input type="submit" value="Idle"></form>');
+});
+
+app.post('/idle', function (req, res) {
+    // cpt = cpt + 1;
+    // console.log(cpt);
+    res.send('Idle <form action="/dance" method="post"><input type="submit" value="Dance"></form>');
+});
+
+var server = app.listen(5000, function () {
+    console.log('Node server is running..');
+});
